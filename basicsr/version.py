bac58a2b945792f286555b17e769db65ee07754b
Evgeney Zimin <evgeneyz@vg-gpu-01.lab.graphicon.ru>
2022-02-18 00:54:29 +0300

    Test commit

--- basicsr/version.py
+++ basicsr/version.py
@@ -1,5 +1,5 @@
 # GENERATED VERSION FILE
-# TIME: Tue Jan 25 18:53:38 2022
+# TIME: Sat Feb 12 11:37:57 2022
 __version__ = '1.3.4.9'
 __gitsha__ = 'unknown'
 version_info = (1, 3, 4, 9)
--- run.sh
+++ run.sh
@@ -1,6 +1,24 @@
 cd /model
 python3 setup.py develop
-mkdir /results/gauss
-python3 /model/inference/inference_basicvsr.py --input_path /dataset/gauss --save_path /results/gauss
-mkdir /results/bicubic
-python3 /model/inference/inference_basicvsr.py --input_path /dataset/bicubic --save_path /results/bicubic
+mkdir /results/shakewalk@uavs3e@b150
+python3 /model/inference/inference_basicvsr.py --input_path /dataset/shakewalk@uavs3e@b150 --save_path /results/shakewalk@uavs3e@b150
+mkdir /results/shakewalk@aomenc@b172
+python3 /model/inference/inference_basicvsr.py --input_path /dataset/shakewalk@aomenc@b172 --save_path /results/shakewalk@aomenc@b172
+mkdir /results/shakewalk@uavs3e@b4234
+python3 /model/inference/inference_basicvsr.py --input_path /dataset/shakewalk@uavs3e@b4234 --save_path /results/shakewalk@uavs3e@b4234
+mkdir /results/shakewalk@uavs3e@b82
+python3 /model/inference/inference_basicvsr.py --input_path /dataset/shakewalk@uavs3e@b82 --save_path /results/shakewalk@uavs3e@b82
+mkdir /results/shakewalk@uavs3e@b1035
+python3 /model/inference/inference_basicvsr.py --input_path /dataset/shakewalk@uavs3e@b1035 --save_path /results/shakewalk@uavs3e@b1035
+mkdir /results/shakewalk@aomenc@b493
+python3 /model/inference/inference_basicvsr.py --input_path /dataset/shakewalk@aomenc@b493 --save_path /results/shakewalk@aomenc@b493
+mkdir /results/shakewalk@aomenc@b1656
+python3 /model/inference/inference_basicvsr.py --input_path /dataset/shakewalk@aomenc@b1656 --save_path /results/shakewalk@aomenc@b1656
+mkdir /results/shakewalk@uavs3e@b518
+python3 /model/inference/inference_basicvsr.py --input_path /dataset/shakewalk@uavs3e@b518 --save_path /results/shakewalk@uavs3e@b518
+mkdir /results/shakewalk@aomenc@b992
+python3 /model/inference/inference_basicvsr.py --input_path /dataset/shakewalk@aomenc@b992 --save_path /results/shakewalk@aomenc@b992
+mkdir /results/shakewalk@uavs3e@b2065
+python3 /model/inference/inference_basicvsr.py --input_path /dataset/shakewalk@uavs3e@b2065 --save_path /results/shakewalk@uavs3e@b2065
+mkdir /results/shakewalk@aomenc@b3629
+python3 /model/inference/inference_basicvsr.py --input_path /dataset/shakewalk@aomenc@b3629 --save_path /results/shakewalk@aomenc@b3629
